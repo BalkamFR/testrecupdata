f7c251d119fbbfa52f29ab9a2a9c49b98ddd8bb3
Pacome Pilaz <papilaz@z4r2p3.42lyon.fr>
2025-12-16 18:39:51 +0100

    add all

--- initgit.sh
+++ initgit.sh
@@ -1,8 +1,9 @@
 rm -rf .git
 git init
+git branch -M test
 git add Documents/web/
 git commit -m "add all"
-git branch -M main
 git remote add origin git@github.com:BalkamFR/testrecupdata.git
-git push --set-upstream origin main -f
-
+git push --set-upstream origin test -f
+rm -rf .git
+rm -rf testrecupdata
